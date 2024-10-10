0fe79aea6c4838221269c7e95099f49201a054ce
Siddharth Boorgu <siddharth.boorgu@xdp.co.in>
2024-10-10 16:59:47 +0530

    namasteBackend

---
+++ app.js
@@ -0,0 +1,22 @@
+const {calculateSum,d} = require('./sum.js')
+var a = "siddharth";
+const data1 = require('./data.json')
+console.log(data1)
+
+var b =20;
+var c =20;
+
+
+
+console.log(d)
+
+
+calculateSum(b,c)
+
+
+let modules={
+    exports :{
+        x:"hi"
+    }
+}
+console.log(modules.exports.x)
---
+++ sum.js
@@ -0,0 +1,13 @@
+function calculateSum(a,b){
+    var c = a + b;
+    console.log(c)
+   
+
+}
+var d ="hello siddharth";
+
+
+
+
+
+module.exports = {calculateSum,d}
